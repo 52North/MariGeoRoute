21f16f4049e1a42fd9f05aa236b1094c35787e79
Katharina Demmich <k.demmich@52north.org>
2023-03-01 16:19:36 +0100

    move from flask API to pure python package - implement RoutingAlgFactory class which initialises the routing algorithm from file

--- Isochrone/app.py
+++ Isochrone/app.py
@@ -1,64 +1,30 @@
-"""Flask main."""
 import io
 import logging
-import graphics
-from logging import FileHandler, Formatter
-from flask import Flask, Response, render_template, request, send_from_directory
-from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
-import logging
-import logging.handlers
 import os
 import warnings
+import logging.handlers
+from logging import FileHandler, Formatter
 
-import router
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+
+import config
+import graphics
 from polars import *
 from weather import *
 from Constraints import *
+from RoutingAlgFactory import *
 
-# App Config.
-app = Flask(__name__)
-app.config.from_object('config')
+def merge_figures_to_gif(path, nof_figures):
+    graphics.merge_figs(path, nof_figures)
 
-state = {}
-state['hour'] = 0
-
-'''
-# Controllers.
-@app.route('/', methods=["GET", "POST"])
-def home():
-    """Route handling."""
-
-    if request.method == 'GET':
-        return render_template(
-            'pages/placeholder.home.html',
-            hour=state['hour'])
-    if request.method == 'POST':
-        if 'increment' in request.form:
-            state['hour'] = state['hour'] + 3
-        elif 'decrement' in request.form:
-            state['hour'] = state['hour'] - 3
-        return render_template(
-            'pages/placeholder.home.html',
-            hour=state['hour'])
-
-
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static/ico'),
-                               'favicon.png', mimetype='image/vnd.microsoft.icon')
-
-'''
-
-
-@app.route('/')
-def plot_map():
+if __name__ == "__main__":
     ##
     # initialise logging
     logger = logging.getLogger('WRT')
     logger.setLevel(logging.INFO)
-    fh = logging.FileHandler(app.config['PERFORMANCE_LOG_FILE'], mode='w')
+    fh = logging.FileHandler(config.PERFORMANCE_LOG_FILE, mode='w')
     fh.setLevel(logging.WARNING)
-    fhinfo = logging.FileHandler(app.config['INFO_LOG_FILE'], mode='w')
+    fhinfo = logging.FileHandler(config.INFO_LOG_FILE, mode='w')
     fhinfo.setLevel(logging.INFO)
     formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
     fh.setFormatter(formatter)
@@ -70,45 +36,24 @@ def plot_map():
     # suppress warnings from mariPower
     warnings.filterwarnings("ignore")
 
-    #graphics.merge_figs("/home/kdemmich/MariData/Code/Figures/Depth10m/", 5)
-
-    """Route handling."""
-    try:
-        lat1 = request.args['lat1']
-        lon1 = request.args['lon1']
-        lat2 = request.args['lat2']
-        lon2 = request.args['lon2']
-    except (KeyError):
-        lat1, lon1, lat2, lon2 = app.config['DEFAULT_MAP']
-        dpi = app.config['DPI']
-        logger.info('using default map coordinates')
-
-    try:
-        lat1 = float(lat1)
-        lat2 = float(lat2)
-        lon1 = float(lon1)
-        lon2 = float(lon2)
-    except (ValueError):
-        raise ValueError('Expecting real values for map coordinates')
-
     # *******************************************
     # basic settings
-    model = app.config['DEFAULT_GFS_MODEL']
-    boatfile = app.config['DEFAULT_BOAT']
-    windfile = app.config['DEFAULT_GFS_FILE']
-    depthfile = app.config['DEFAULT_DEPTH_FILE']
-    coursesfile = app.config['COURSES_FILE']
-    figurepath = app.config['FIGURE_PATH']
-    delta_time = app.config['DELTA_TIME_FORECAST']
-    delta_fuel = app.config['DELTA_FUEL']
-    hours = app.config['TIME_FORECAST']
-    routing_steps = app.config['ROUTING_STEPS']
-    lat1, lon1, lat2, lon2 = app.config['DEFAULT_MAP']
-    r_la1, r_lo1, r_la2, r_lo2 = app.config['DEFAULT_ROUTE']
+    model = config.DEFAULT_GFS_MODEL
+    boatfile = config.DEFAULT_BOAT
+    windfile = config.DEFAULT_GFS_FILE
+    depthfile = config.DEFAULT_DEPTH_FILE
+    coursesfile = config.COURSES_FILE
+    figurepath = config.FIGURE_PATH
+    delta_time = config.DELTA_TIME_FORECAST
+    delta_fuel = config.DELTA_FUEL
+    hours = config.TIME_FORECAST
+    routing_steps = config.ROUTING_STEPS
+    lat1, lon1, lat2, lon2 = config.DEFAULT_MAP
+    r_la1, r_lo1, r_la2, r_lo2 = config.DEFAULT_ROUTE
     start = (r_la1, r_lo1)
     finish = (r_la2, r_lo2)
-    start_time = dt.datetime.strptime(app.config['DEFAULT_GFS_DATETIME'], '%Y%m%d%H')
-    params = app.config
+    start_time = dt.datetime.strptime(config.DEFAULT_GFS_DATETIME, '%Y%m%d%H')
+    dpi = config.DPI
 
     # *******************************************
     # initialise boat
@@ -151,83 +96,20 @@ def plot_map():
     constraint_list.add_neg_constraint(water_depth)
     constraint_list.print_settings()
 
-    '''min_time_route = router.modified_isochrone_routing(
-        start, finish,
-        boat,
-        wt,
-        start_time,
-        delta_time, routing_steps,
-        params,
-        fig
-    )
-    fig = min_time_route['fig']'''
+    # *******************************************
+    # initialise rout
+    route_factory = RoutingAlgFactory()
+    min_fuel_route = route_factory.get_routing_alg('ISOFUEL')
+    min_fuel_route.init_fig(wt)
 
-    min_fuel_route = router.min_fuel_routing(
-        start, finish,
-        boat,
-        wt,
-        constraint_list,
-        start_time,
-        delta_fuel, routing_steps,
-        params,
-        figurepath
-    )
-
-    # if not (min_fuel_route.__eq__(min_time_route)):
-    #    raise ValueError('Routes not matching!')'''
-
-    # fig =  graphics.create_map(lat1, lon1, lat2, lon2, dpi)
-    #   # fig = graphics.plot_route(fig, min_time_route['route'], graphics.get_colour(1))
-    # fig = graphics.plot_route(fig, min_fuel_route, graphics.get_colour(1))
-    # fig = graphics.plot_legend(fig)
-
-    # graphics.plot_power_vs_dist(min_fuel_route['route'])
-
-    # output = io.BytesIO()
-    # FigureCanvas(fig).print_png(output)
-
+    # *******************************************
+    # routing
+    min_fuel_route = min_fuel_route.recursive_routing(boat, wt, constraint_list)
+    min_fuel_route.print_route()
     min_fuel_route.write_to_file(str(min_fuel_route.route_type) + "route.json")
 
+    # *******************************************
     # plot route in constraints
     fig, ax = plt.subplots(figsize=(12, 7))
     water_depth.plot_route_in_constraint(min_fuel_route, graphics.get_colour(1), fig, ax)
     plt.savefig(figurepath + '/route_waterdepth.png')
-
-    return 0  # Response(output.getvalue(), mimetype='image/png')
-
-
-# Error handlers.
-@app.errorhandler(500)
-def internal_error(error):
-    """Error handling."""
-    # return render_template('errors/500.html'), 500
-    return 'Error 500'
-
-
-@app.errorhandler(404)
-def not_found_error(error):
-    """Error handling."""
-    # return render_template('errors/404.html'), 404
-    return 'Error 404'
-
-
-if not app.debug:
-    file_handler = FileHandler('error.log')
-    fmt = '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'
-    file_handler.setFormatter(Formatter(fmt))
-    app.logger.setLevel(logging.INFO)
-    file_handler.setLevel(logging.INFO)
-    app.logger.addHandler(file_handler)
-    app.logger.info('errors')
-
-# Default port:
-if __name__ == '__main__':
-    # app.run()
-    app.run(host="localhost", port=5000, debug=True)
-
-# Or specify port manually:
-'''
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
-'''
