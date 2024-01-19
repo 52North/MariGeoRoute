1efcf32ca889af17796c00a481ee14415103e5a8
Martin Pontius <m.pontius@52north.org>
2024-01-19 14:11:07 +0100

    Delete maridatadownloader

--- data/maridatadownloader/maridatadownloader/base.py
+++
@@ -1,9 +0,0 @@
-class DownloaderBase:
-    """Downloader base class"""
-    def __init__(self, downloader_type, **kwargs):
-        self.downloader_type = downloader_type
-        self.username = kwargs.get('username', None)
-        self.password = kwargs.get('password', None)
-
-    def download(self, **kwargs):
-        raise NotImplementedError(".download() must be overridden.")
